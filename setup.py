10d663c1cf6f3c0aafec9cc61054ac7bcf907054
Jordan Juravsky <jordan@mindcharger.com>
2025-01-25 17:08:32 -0800

    bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     setup(
         name="pydra-config",
-        version="0.0.6",
+        version="0.0.7",
         packages=["pydra"],
         author="Jordan Juravsky",
         url="https://github.com/jordan-benjamin/pydra",
