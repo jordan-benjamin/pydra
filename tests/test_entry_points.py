7e2ae95acc3ffce2639078959110728eb7d6d51c
Jordan Juravsky <jordan@mindcharger.com>
2025-01-25 20:03:12 -0800

    tests for entry points (pydra.main and pydra.run)

---
+++ tests/test_entry_points.py
@@ -0,0 +1,27 @@
+import unittest
+from pydra import Config, main, run
+
+
+class SimpleConfig(Config):
+    def __init__(self):
+        self.x = 1
+        self.y = 2
+
+
+def undecorated_fn(config: SimpleConfig):
+    return config.x * config.y + 1
+
+
+@main(SimpleConfig)
+def decorated_fn(config: SimpleConfig):
+    return undecorated_fn(config)
+
+
+class TestEntryPoints(unittest.TestCase):
+    def test_run(self):
+        result = run(undecorated_fn, ["x=5"])
+        self.assertEqual(result, 11)
+
+    def test_main(self):
+        result = decorated_fn(["x=5", "y=3"])
+        self.assertEqual(result, 16)
